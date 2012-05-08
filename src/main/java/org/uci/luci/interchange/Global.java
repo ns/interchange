31d28ea9be77940939c21cae1793bdd61803083f
cheruvian <ryan0rules>
2012-05-08 13:56:30 -0700

    Added Projection and Bounding Lat/Long to Global

--- src/main/java/org/uci/luci/interchange/Global.java
+++ src/main/java/org/uci/luci/interchange/Global.java
@@ -2,4 +2,10 @@ package org.uci.luci.interchange;
 
 public class Global {
   public static OpenStreetMap openStreetMap;
+  public static Projection projection = new MercatorProjection();
+  public static double maxLat;
+  public static double minLat;
+  public static double maxLon;
+  public static double minLon;
+
 }
