2c26b87af5c9070246df0538e81c07f56e8a9a00
cheruvian <ryan0rules>
2012-06-05 02:29:08 -0700

    Track AppWindow in Global

--- src/main/java/org/uci/luci/interchange/Util/Global.java
+++ src/main/java/org/uci/luci/interchange/Util/Global.java
@@ -1,11 +1,13 @@
 package org.uci.luci.interchange.Util;
 
 import org.uci.luci.interchange.OSM.*;
+import org.uci.luci.interchange.UI.AppWindow;
 import org.uci.luci.interchange.Simulator;
 
 public class Global {
 	public static OpenStreetMap openStreetMap;
 	public static Simulator simulator;
+	public static AppWindow window;
 
 	public static Projection projection = new MercatorProjection();
 	public static double maxLat;
